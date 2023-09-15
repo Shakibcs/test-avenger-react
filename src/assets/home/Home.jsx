d9d8e7b9e4a8a8e3f8e81a8f82868c9c29c9df42
Shakibul Alam <droid@Shakibuls-Air.station>
2023-09-16 00:41:13 +0100

    used map function to get data and displayed it

--- src/assets/home/Home.jsx
+++ src/assets/home/Home.jsx
@@ -1,29 +1,42 @@
-import { useEffect } from 'react';
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from 'react';
 import './Home.css'
 const Home = () => {
+
+    const [allActors,setAllActors] = useState([])
 
     useEffect(()=>{
         fetch('../../../data.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setAllActors(data))
     },[])
 
     return (
         <div className="container">
-            <div className="cart-container">
-                <div className="cart">
-                <div className="cart-img">
-                  <img className="photo" src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" />
-                </div>
-                <h2>Anton Tech</h2>
-                <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, laudantium.</small></p>
-                <div className="info">
-                <p>Salary: 5000</p>
-                <p>Writer: </p>
-                </div>
-                <button className='btn btn-primary mt-6'>Select</button>
-                </div>
+
+            <div className="home-container">
+               {
+               allActors.map(actor => (
+                <div key={actor.id} className="cart">
+                    <div className="cart-img">
+                      <img className="photo" src={actor.image} alt="" />
+                    </div>
+                    <h2>Name: {actor.name}</h2>
+                    <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, laudantium.</small></p>
+                    <div className="info">
+                    <p>Salary: {actor.salary}</p>
+                    <p>Writer: {actor.role}</p>
+                    </div>
+                    <button className='btn btn-primary mt-10'>Select</button>
+                    </div>
+               ))
+               }
             </div>
+
+            <div className="side-cart">
+                <h2>This is side container</h2>
+            </div>
+
         </div>
     );
 };
